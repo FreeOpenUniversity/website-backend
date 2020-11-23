d42d0e9161148941ba042e753059ecc067e7c064
tas09009 <taniya.singh12@gmail.com>
2020-11-23 15:44:17 -0500

    subcategory changes thus far. Need to test

--- src/main/java/com/free_open_university/backend/bean/SubCategory.java
+++ src/main/java/com/free_open_university/backend/bean/SubCategory.java
@@ -1,4 +1,3 @@
-/*
 package com.free_open_university.backend.bean;
 
 import javax.persistence.*;
@@ -9,13 +8,78 @@ public class SubCategory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
 
-    @Column(name = "Name")
-    private String name;
+    private int subid;
+    // @Column(name = "Name")
+    private String subname;
+    private int catid;
 
-    @ManyToOne
-    @JoinColumn(name = "CategoryId", referencedColumnName = "id")
+    // @Column(name = "SubCategoryId")
+    // private int 
+    // @Column(name = "SubCategoryName")
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CategoryId", referencedColumnName =  "id")
     private Category category;
+
+    public SubCategory(int subid, String subname, int catid) {
+        super();
+        this.subid = subid;
+        this.subname = subname;
+        this.catid = catid;
+    }
+
+    public SubCategory( ) {
+        super();
+    }
+
+    public int getSubid( ) {
+        return subid;
+    }
+
+    public void setSubid(int subid) {
+        this.subid = subid;
+    }   
+
+    public String getSubname( ) {
+        return subname;
+    }
+
+    public void setSubname(String subname) {
+        this.subname = subname;
+    }
+
+    public int getCatid( ) {
+        return catid;
+    }
+
+    public void setCatid(int catid) {
+        this.catid = catid;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
 }
+
+/*
+@JoinColumn(name = "CategoryId", referencedColumnName =  "id")
+private Category category;
+
+public String getName() {
+    return name;
+}
+
+public List<SubCategory> getSubCategoryList() {
+    return subCategoryList;
+}
+
+public void setSubCategoryList(List<SubCategory> subCategoryList) {
+    this.subCategoryList = subCategoryList;     
+}   
 */
