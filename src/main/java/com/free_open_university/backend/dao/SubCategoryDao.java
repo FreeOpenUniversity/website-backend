acbf016827b2520597ff38134490fa4b460eee6b
Taniya Singh <taniya.singh12@gmail.com>
2020-11-25 14:34:17 -0500

    Revert "Develop"

--- src/main/java/com/free_open_university/backend/dao/SubCategoryDao.java
+++
@@ -1,8 +0,0 @@
-/*
-package com.free_open_university.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubCategoryDao extends JpaRepository<SubCategoryDao, Integer> {
-}
-*/
